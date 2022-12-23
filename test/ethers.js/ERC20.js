9ebbf12fd70b1aa6a3c87c27b9933b7d89a3aca1
ychiao <ychiaoli18@users.noreply.github.com>
2022-12-24 00:30:16 +0800

    fix ethers.js/ERC20 tests

--- test/ethers.js/ERC20.js
+++ test/ethers.js/ERC20.js
@@ -10,40 +10,52 @@ const TOKEN_INITIAL_SUPPLY = 1000
 let deployerAddr, deploymentTxHash, erc20Address
 
 describe('ERC20', function () {
-  it('Should successfully deploy', async function () {
-    deployerAddr = await getDeployerAddress()
-    const erc20 = await deployContract('ERC20PresetFixedSupply',
-      TOKEN_NAME, TOKEN_SYMBOL, TOKEN_INITIAL_SUPPLY, deployerAddr)
+  it("Should successfully deploy", async function () {
+    deployerAddr = await getDeployerAddress();
+    const erc20 = await deployContract(
+      "ERC20PresetFixedSupply",
+      TOKEN_NAME,
+      TOKEN_SYMBOL,
+      TOKEN_INITIAL_SUPPLY,
+      deployerAddr
+    );
 
-    deploymentTxHash = erc20.deployTransaction.hash
+    deploymentTxHash = erc20.deployTransaction.hash;
+    console.log("hash", deploymentTxHash);
 
-    await erc20.deployed()
-  })
-  it('Should access deployment transaction receipt',
-    async function () {
-      const txReceipt = await ethers.provider.getTransactionReceipt(
-        deploymentTxHash)
-      erc20Address = txReceipt.contractAddress
-    })
-  it('Should set the right name', async function () {
-    const ERC20 = await ethers.getContractAt('ERC20PresetFixedSupply',
-      erc20Address)
-    const name = await ERC20.name()
+    await erc20.deployed();
+  });
+  it("Should access deployment transaction receipt", async function () {
+    const txReceipt = await ethers.provider.getTransactionReceipt(
+      deploymentTxHash
+    );
+    erc20Address = txReceipt.contractAddress;
+  });
+  it("Should set the right name", async function () {
+    const ERC20 = await ethers.getContractAt(
+      "ERC20PresetFixedSupply",
+      erc20Address
+    );
+    const name = await ERC20.name();
 
-    name.should.be.equal(TOKEN_NAME)
-  })
-  it('Should set the right symbol', async function () {
-    const ERC20 = await ethers.getContractAt('ERC20PresetFixedSupply',
-      erc20Address)
-    const symbol = await ERC20.symbol()
+    name.should.be.equal(TOKEN_NAME);
+  });
+  it("Should set the right symbol", async function () {
+    const ERC20 = await ethers.getContractAt(
+      "ERC20PresetFixedSupply",
+      erc20Address
+    );
+    const symbol = await ERC20.symbol();
 
-    symbol.should.be.equal(TOKEN_SYMBOL)
-  })
-  it('Should set the right initial supply', async function () {
-    const ERC20 = await ethers.getContractAt('ERC20PresetFixedSupply',
-      erc20Address)
-    const ownerBalance = await ERC20.balanceOf(deployerAddr)
+    symbol.should.be.equal(TOKEN_SYMBOL);
+  });
+  it("Should set the right initial supply", async function () {
+    const ERC20 = await ethers.getContractAt(
+      "ERC20PresetFixedSupply",
+      erc20Address
+    );
+    const ownerBalance = await ERC20.balanceOf(deployerAddr);
 
-    ownerBalance.should.be.equal(TOKEN_INITIAL_SUPPLY)
-  })
+    ownerBalance.should.be.equal(TOKEN_INITIAL_SUPPLY);
+  });
 })
