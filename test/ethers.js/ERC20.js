d6f32a27990908bd652bf88aaad4cd834d014403
ychiao <ychiaoli18@users.noreply.github.com>
2023-02-08 14:42:59 -0500

    itest: implement event tests using ethers.js and web3.js (#19)

--- test/ethers.js/ERC20.js
+++ test/ethers.js/ERC20.js
@@ -1,15 +1,25 @@
-require('chai').should()
-const { ethers } = require('hardhat')
-const deployContract = require('./utils/deployContract')
-const { getDeployerAddress } = require('./utils/getDeployerAddresses')
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+chai.should();
+chai.use(sinonChai);
 
-const TOKEN_NAME = 'my_token'
-const TOKEN_SYMBOL = 'TKN'
-const TOKEN_INITIAL_SUPPLY = 1000
+const { ethers } = require("hardhat");
+const deployContract = require("./utils/deployContract");
+const { getDeployerAddress } = require("./utils/getDeployerAddresses");
 
-let deployerAddr, deploymentTxHash, erc20Address
+const WebSocket = require("ws");
 
-describe('ERC20', function () {
+const { Formatter } = require("@ethersproject/providers");
+
+const TOKEN_NAME = "my_token";
+const TOKEN_SYMBOL = "TKN";
+const TOKEN_INITIAL_SUPPLY = 1000;
+
+let deployerAddr, deploymentTxHash, erc20Address;
+
+describe("ERC20", function () {
   it("Should successfully deploy", async function () {
     deployerAddr = await getDeployerAddress();
     const erc20 = await deployContract(
@@ -58,4 +68,140 @@ describe('ERC20', function () {
 
     ownerBalance.should.be.equal(TOKEN_INITIAL_SUPPLY);
   });
-})
+  it("Should be able to transfer token", async function () {
+    const [deployer, receiver] = await web3.eth.getAccounts();
+    const ERC20 = await ethers.getContractAt(
+      "ERC20PresetFixedSupply",
+      erc20Address
+    );
+
+    const wsUrl = hre.network.config.url.replace("http", "ws");
+    const wsProvider = new ethers.providers.WebSocketProvider(wsUrl);
+
+    function transferEventAssertFunc(event) {
+      event.address.should.equal(erc20Address);
+      event.topics[0].should.equal(
+        ethers.utils.id("Transfer(address,address,uint256)")
+      );
+      event.topics[1].should.equal(
+        ethers.utils.hexZeroPad(deployer, 32).toLowerCase()
+      );
+      event.topics[2].should.equal(
+        ethers.utils.hexZeroPad(receiver, 32).toLowerCase()
+      );
+    }
+
+    const spy0 = sinon.spy();
+    wsProvider.on("block", (blockNumber) => {
+      spy0();
+      expect(blockNumber).to.be.a("number");
+    });
+
+    const spy1 = subscribe(
+      wsProvider,
+      {
+        address: erc20Address,
+      },
+      transferEventAssertFunc
+    );
+
+    const spy2 = subscribe(
+      wsProvider,
+      {
+        address: web3.eth.accounts.create().address,
+      },
+      transferEventAssertFunc
+    );
+
+    const spy3 = subscribe(
+      wsProvider,
+      {
+        topics: null,
+      },
+      transferEventAssertFunc
+    );
+
+    const spy4 = subscribe(
+      wsProvider,
+      {
+        topics: [],
+      },
+      transferEventAssertFunc
+    );
+
+    const spy5 = subscribe(
+      wsProvider,
+      {
+        topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+      },
+      transferEventAssertFunc
+    );
+
+    const spy6 = subscribe(
+      wsProvider,
+      {
+        topics: [ethers.utils.id("NOTEXIST(address)")],
+      },
+      transferEventAssertFunc
+    );
+
+    const spy7 = subscribe(
+      wsProvider,
+      {
+        address: erc20Address,
+        topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+      },
+      transferEventAssertFunc
+    );
+
+    const spy8 = subscribe(
+      wsProvider,
+      {
+        address: erc20Address,
+        topics: [ethers.utils.id("NOTEXIST(address)")],
+      },
+      transferEventAssertFunc
+    );
+
+    const spy9 = subscribe(
+      wsProvider,
+      {
+        address: erc20Address,
+        topics: [null, ethers.utils.hexZeroPad(deployer, 32).toLowerCase()],
+      },
+      transferEventAssertFunc
+    );
+
+    const maxPriorityFeePerGas = parseInt(
+      await ethers.provider.send("eth_maxPriorityFeePerGas", [])
+    );
+    const options = { maxPriorityFeePerGas };
+
+    const receipt = await ERC20.transfer(
+      receiver,
+      TOKEN_INITIAL_SUPPLY,
+      options
+    );
+    await receipt.wait();
+
+    spy0.should.have.been.called;
+    spy1.should.have.been.calledOnce;
+    spy2.should.not.have.been.called;
+    spy3.should.have.been.calledOnce;
+    spy4.should.have.been.calledOnce;
+    spy5.should.have.been.calledOnce;
+    spy6.should.not.have.been.called;
+    spy7.should.have.been.calledOnce;
+    spy8.should.not.have.been.called;
+    spy9.should.have.been.calledOnce;
+  });
+});
+
+function subscribe(provider, filter, assertFunc) {
+  const spy = sinon.spy();
+  provider.on(filter, (event) => {
+    spy();
+    assertFunc(event);
+  });
+  return spy;
+}
--- test/web3.js/ERC20.js
+++ test/web3.js/ERC20.js
@@ -1,44 +1,52 @@
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require("chai");
 const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
 chai.should();
 chai.use(sinonChai);
 
-const deployContract = require('./utils/deployContract')
-const { getDeployerAddress } = require('./utils/getDeployerAddresses')
+const deployContract = require("./utils/deployContract");
+const { getDeployerAddress } = require("./utils/getDeployerAddresses");
+
 const hre = require("hardhat");
+const { Formatter } = require("@ethersproject/providers");
 
-const TOKEN_NAME = 'my_token'
-const TOKEN_SYMBOL = 'TKN'
-const TOKEN_INITIAL_SUPPLY = 1000
+const TOKEN_NAME = "my_token";
+const TOKEN_SYMBOL = "TKN";
+const TOKEN_INITIAL_SUPPLY = 1000;
 
-let deployerAddr, erc20
+let deployerAddr, erc20;
 
-describe('ERC20', function () {
-  it('Should successfully deploy', async function () {
-    deployerAddr = await getDeployerAddress()
-    const { contract } = await deployContract('ERC20PresetFixedSupply',
-      TOKEN_NAME, TOKEN_SYMBOL, TOKEN_INITIAL_SUPPLY, deployerAddr)
+describe("ERC20", function () {
+  it("Should successfully deploy", async function () {
+    deployerAddr = await getDeployerAddress();
+    const { contract } = await deployContract(
+      "ERC20PresetFixedSupply",
+      TOKEN_NAME,
+      TOKEN_SYMBOL,
+      TOKEN_INITIAL_SUPPLY,
+      deployerAddr
+    );
 
-    erc20 = await contract
-  })
-  it('Should set the right name', async function () {
-    const name = await erc20.methods.name().call()
+    erc20 = await contract;
+  });
+  it("Should set the right name", async function () {
+    const name = await erc20.methods.name().call();
 
-    name.should.be.equal(TOKEN_NAME)
-  })
-  it('Should set the right symbol', async function () {
-    const symbol = await erc20.methods.symbol().call()
+    name.should.be.equal(TOKEN_NAME);
+  });
+  it("Should set the right symbol", async function () {
+    const symbol = await erc20.methods.symbol().call();
 
-    symbol.should.be.equal(TOKEN_SYMBOL)
-  })
-  it('Should set the right initial supply', async function () {
-    const ownerBalance = Number(await erc20.methods.balanceOf(deployerAddr).call())
+    symbol.should.be.equal(TOKEN_SYMBOL);
+  });
+  it("Should set the right initial supply", async function () {
+    const ownerBalance = Number(
+      await erc20.methods.balanceOf(deployerAddr).call()
+    );
 
-    ownerBalance.should.be.equal(TOKEN_INITIAL_SUPPLY)
-  })
-  it('Should be able to transfer token', async function () {
+    ownerBalance.should.be.equal(TOKEN_INITIAL_SUPPLY);
+  });
+  it("Should be able to transfer token", async function () {
     const [deployer, receiver] = await web3.eth.getAccounts();
 
     const wsUrl = hre.network.config.url.replace("http", "ws");
@@ -46,51 +54,102 @@ describe('ERC20', function () {
 
     function transferEventAssertFunc(event) {
       event.address.should.equal(erc20.options.address);
-      event.topics[0].should.equal(ethers.utils.id("Transfer(address,address,uint256)"));
-      event.topics[1].should.equal(ethers.utils.hexZeroPad(deployer, 32).toLowerCase());
-      event.topics[2].should.equal(ethers.utils.hexZeroPad(receiver, 32).toLowerCase());
+      event.topics[0].should.equal(
+        ethers.utils.id("Transfer(address,address,uint256)")
+      );
+      event.topics[1].should.equal(
+        ethers.utils.hexZeroPad(deployer, 32).toLowerCase()
+      );
+      event.topics[2].should.equal(
+        ethers.utils.hexZeroPad(receiver, 32).toLowerCase()
+      );
     }
 
-    const spy1 = subscribe(wsWeb3, {
-      address: erc20.options.address,
-    }, transferEventAssertFunc);
+    const spy0 = sinon.spy();
+    const subscription = wsWeb3.eth.subscribe("newBlockHeaders");
+    subscription.on("data", (block) => {
+      Formatter.check(new Formatter().formats.block, block);
+      spy0();
+    });
 
-    const spy2 = subscribe(wsWeb3, {
-      address: web3.eth.accounts.create().address,
-    }, transferEventAssertFunc);
+    const spy1 = subscribe(
+      wsWeb3,
+      {
+        address: erc20.options.address,
+      },
+      transferEventAssertFunc
+    );
 
-    const spy3 = subscribe(wsWeb3, {
-      topics: null,
-    }, transferEventAssertFunc);
+    const spy2 = subscribe(
+      wsWeb3,
+      {
+        address: web3.eth.accounts.create().address,
+      },
+      transferEventAssertFunc
+    );
 
-    const spy4 = subscribe(wsWeb3, {
-      topics: [],
-    }, transferEventAssertFunc);
+    const spy3 = subscribe(
+      wsWeb3,
+      {
+        topics: null,
+      },
+      transferEventAssertFunc
+    );
 
-    const spy5 = subscribe(wsWeb3, {
-      topics: [ethers.utils.id("Transfer(address,address,uint256)")],
-    }, transferEventAssertFunc);
+    const spy4 = subscribe(
+      wsWeb3,
+      {
+        topics: [],
+      },
+      transferEventAssertFunc
+    );
 
-    const spy6 = subscribe(wsWeb3, {
-      topics: [ethers.utils.id("NOTEXIST(address)")],
-    }, transferEventAssertFunc);
+    const spy5 = subscribe(
+      wsWeb3,
+      {
+        topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+      },
+      transferEventAssertFunc
+    );
 
-    const spy7 = subscribe(wsWeb3, {
-      address: erc20.options.address,
-      topics: [ethers.utils.id("Transfer(address,address,uint256)")],
-    }, transferEventAssertFunc);
+    const spy6 = subscribe(
+      wsWeb3,
+      {
+        topics: [ethers.utils.id("NOTEXIST(address)")],
+      },
+      transferEventAssertFunc
+    );
 
-    const spy8 = subscribe(wsWeb3, {
-      address: erc20.options.address,
-      topics: [ethers.utils.id("NOTEXIST(address)")],
-    }, transferEventAssertFunc);
+    const spy7 = subscribe(
+      wsWeb3,
+      {
+        address: erc20.options.address,
+        topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+      },
+      transferEventAssertFunc
+    );
 
-    const spy9 = subscribe(wsWeb3, {
-      address: erc20.options.address,
-      topics: [null, ethers.utils.hexZeroPad(deployer, 32).toLowerCase()],
-    }, transferEventAssertFunc);
+    const spy8 = subscribe(
+      wsWeb3,
+      {
+        address: erc20.options.address,
+        topics: [ethers.utils.id("NOTEXIST(address)")],
+      },
+      transferEventAssertFunc
+    );
 
-    await erc20.methods.transfer(receiver, TOKEN_INITIAL_SUPPLY/10).send({from: deployer});
+    const spy9 = subscribe(
+      wsWeb3,
+      {
+        address: erc20.options.address,
+        topics: [null, ethers.utils.hexZeroPad(deployer, 32).toLowerCase()],
+      },
+      transferEventAssertFunc
+    );
+
+    await erc20.methods
+      .transfer(receiver, TOKEN_INITIAL_SUPPLY / 10)
+      .send({ from: deployer });
 
     // not implemented in lotus yet
     // const spy = subscribe(wsWeb3, {
@@ -98,6 +157,7 @@ describe('ERC20', function () {
     //   fromBlock: 0,
     // }, transferEventAssertFunc);
 
+    spy0.should.have.been.called;
     spy1.should.have.been.calledOnce;
     spy2.should.not.have.been.called;
     spy3.should.have.been.calledOnce;
@@ -110,11 +170,10 @@ describe('ERC20', function () {
   });
 });
 
-
 function subscribe(wsWeb3, filter, assertFunc) {
-  const spy = sinon.spy()
-  const subscription = wsWeb3.eth.subscribe('logs', filter);
-  subscription.on('data', (event) => {
+  const spy = sinon.spy();
+  const subscription = wsWeb3.eth.subscribe("logs", filter);
+  subscription.on("data", (event) => {
     spy();
     assertFunc(event);
   });
