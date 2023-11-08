d1e4e4a03c9a7fd3d5f07dad256ada3417d1a7b0
raulk <raul.kripalani@gmail.com>
2023-11-08 23:43:44 +0000

    fixes.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,7 @@ require('@nomiclabs/hardhat-web3')
 require('@nomicfoundation/hardhat-chai-matchers')
 require("hardhat-gas-reporter");
 
-const defaultNodeUrl = "http://localhost:1234/rpc/v0";
-
-const nodeUrl = require('./kit').initNode(1000, 1000);
+const defaultNodeUrl = "http://localhost:8545";
 
 module.exports = {
   solidity: "0.8.15",
@@ -15,21 +13,6 @@ module.exports = {
     local: {
       url: defaultNodeUrl,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-    },
-    itest: {
-      url: nodeUrl + "/rpc/v1",
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY, process.env.USER_1_PRIVATE_KEY],
-    },
-    hyperspace: {
-      url: "http://api.hyperspace.node.glif.io/rpc/v0",
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY, process.env.USER_1_PRIVATE_KEY],
-    },
-    wallaby: {
-      url: "https://wallaby.node.glif.io/rpc/v0",
-      httpHeaders: {
-        "Content-Type": "application/json",
-      },
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY, process.env.USER_1_PRIVATE_KEY],
     },
     hardhat: {
       mining: {
